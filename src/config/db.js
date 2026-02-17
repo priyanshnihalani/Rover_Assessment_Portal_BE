c56c1c76fe3ff73630d9dcd27b380cb93c205409
Priyansh Nihalani <priyansh.nihalani@gmail.com>
2026-02-17 17:09:19 +0530

    [PN][DataBase URL changed]

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "rover_assessment",     
-  "postgres",        
-  "postgres",        
-  {
-    host: "localhost",
-    dialect: "postgres",
-    logging: false    
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
 
 module.exports = sequelize;
